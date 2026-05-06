68b652c8e9a4fdad2f47af27ce43bde92e7e396f
Tsion Bekalu <tsion.ugr-9277-16@aau.edu.et>
2026-05-06 20:12:33 +0300

    Added pathfinding logic with red dot

--- maze.py
+++ maze.py
@@ -43,6 +43,11 @@ def remove_wall(r, c, nr, nc):
     elif nr == r + 1: northWall[r + 1][c] = 0
     elif nr == r - 1: northWall[r][c] = 0
 
+def draw_dot(r, c, color):
+    x, y = (c - C/2) * CELL_SIZE + (CELL_SIZE/2), (r - R/2) * CELL_SIZE + (CELL_SIZE/2)
+    glPointSize(8); glBegin(GL_POINTS); glColor3fv(color)
+    glVertex2f(x, y); glEnd()
+
 def main():
     pygame.init()
     pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
@@ -51,6 +56,11 @@ def main():
     gen_stack = [(random.randint(0, R-1), random.randint(0, C-1))]
     visited_gen[gen_stack[0][0]][gen_stack[0][1]] = True
     gen_done = False
+
+    solver_stack = []
+    visited_solver = [[False for _ in range(C)] for _ in range(R)]
+    start_pos, end_pos = (0,0), (0,0)
+    solving = False
 
     while True:
         for event in pygame.event.get():
@@ -71,8 +81,45 @@ def main():
                 s_r, e_r = random.randint(0, R-1), random.randint(0, R-1)
                 eastWall[s_r][0] = 0   # Entry
                 eastWall[e_r][C] = 0   # Exit
+                start_pos, end_pos = (s_r, 0), (e_r, C-1)
+                solver_stack = [start_pos]
                 gen_done = True
+                solving = True
+        # 2. ANIMATE SOLVER
+        if solving and solver_stack:
+            curr_r, curr_c = solver_stack[-1]
+            visited_solver[curr_r][curr_c] = True
+            
+            if (curr_r, curr_c) == end_pos:
+                solving = False  # We found the exit!
+            else:
+                moved = False
+                # Directions to try: (row change, col change, wall to check)
+                dirs = [(0, 1, 'e'), (1, 0, 'n'), (0, -1, 'w'), (-1, 0, 's')]
+                random.shuffle(dirs) 
+                
+                for dr, dc, wall_type in dirs:
+                    nr, nc = curr_r + dr, curr_c + dc
+                    if 0 <= nr < R and 0 <= nc < C and not visited_solver[nr][nc]:
+                        can_pass = False
+                        # Check if the wall between current and neighbor is broken
+                        if wall_type == 'e' and eastWall[curr_r][curr_c+1] == 0: can_pass = True
+                        elif wall_type == 'w' and eastWall[curr_r][curr_c] == 0: can_pass = True
+                        elif wall_type == 'n' and northWall[curr_r+1][curr_c] == 0: can_pass = True
+                        elif wall_type == 's' and northWall[curr_r][curr_c] == 0: can_pass = True
+                        
+                        if can_pass:
+                            solver_stack.append((nr, nc))
+                            moved = True
+                            break
+                
+                if not moved:
+                    # Backtrack: remove from red path and it will turn blue later
+                    solver_stack.pop()
+        
 
+        if gen_done and solver_stack:
+            for sr, sc in solver_stack: draw_dot(sr, sc, (1, 0, 0))
         pygame.display.flip()
 
 if __name__ == "__main__": main()
